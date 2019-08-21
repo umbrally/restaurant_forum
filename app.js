ada79faa75b49071c86eb8822c3e325252d2084b
Jung Liu <realumbrally@gmail.com>
2019-08-21 16:27:56 +0800

    Add restaurant index page and Add admin restaurant index page

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express()
 const handlebars = require('express-handlebars')
 const port = 3000
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
+
+require('./routes')(app)
