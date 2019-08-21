7ea10e5038d8225dfb2ddc68071d359a9297f930
Jung Liu <realumbrally@gmail.com>
2019-08-21 19:16:52 +0800

    Install bcrypt-nodejs and body-parser and Add userController and signup page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
