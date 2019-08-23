7aaa36d7a4e097ca9e65d8e91179925026b2b1f4
Jung Liu <realumbrally@gmail.com>
2019-08-23 16:18:03 +0800

    Add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -29,6 +29,12 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
   },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant: restaurant })
+    })
+  }
 }
 
 module.exports = adminController
