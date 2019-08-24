dcf21c0e41ce9b93c32c2af8aa332a7fdd5f31af
Jung Liu <realumbrally@gmail.com>
2019-08-24 16:36:23 +0800

    Fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const db = require('./models')
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
