01eea0de74427181879254b4ae21632baf168ad5
Jung Liu <realumbrally@gmail.com>
2019-08-24 16:13:31 +0800

    Add dotenv file and set env variable

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const db = require('./models')
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
