2f549bcd01837d818c76a1b027a3dea3a33024f7
Jung Liu <realumbrally@gmail.com>
2019-08-27 19:02:53 +0800

    Add post comment feature on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController')
 const categoryController = require('../controllers/categoryController.js')
+const commentController = require('../controllers/commentController.js')
 
 
 
@@ -28,6 +29,9 @@ module.exports = (app, passport) => {
   })
   app.get('/restaurants', authenticated, restController.getRestaurants)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+
+  app.post('/comments', authenticated, commentController.postComment)
+
 
   app.get('/admin', authenticatedAdmin, (req, res) => {
     res.redirect('/admin/restaurants')
