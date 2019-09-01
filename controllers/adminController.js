2c6750f9794fee36b43b6316de366104823f4ef7
Jung Liu <realumbrally@gmail.com>
2019-09-01 14:01:55 +0800

    Create fellowship model and add followers and followings to users

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -42,7 +42,8 @@ const adminController = {
           opening_hours: req.body.opening_hours,
           description: req.body.description,
           image: file ? img.data.link : null,
-          CategoryId: req.body.categoryId
+          CategoryId: req.body.categoryId,
+          viewCounts = 0
         }).then((restaurant) => {
           req.flash('success_messages', 'restaurant was successfully created')
           return res.redirect('/admin/restaurants')
@@ -56,7 +57,8 @@ const adminController = {
         opening_hours: req.body.opening_hours,
         description: req.body.description,
         image: null,
-        CategoryId: req.body.categoryId
+        CategoryId: req.body.categoryId,
+        viewCounts = 0
       }).then((restaurant) => {
         req.flash('success_messages', 'restaurant was successfully created')
         return res.redirect('/admin/restaurants')
